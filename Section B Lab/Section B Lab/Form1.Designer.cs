847515ee80782fd0fc2945639e856e99121822c0
Blake McMillan <Blake.McMillan@mymhc.ca>
2023-01-16 13:41:59 -0700

    1

--- Section B Lab/Section B Lab/Form1.Designer.cs
+++ Section B Lab/Section B Lab/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtNames = new System.Windows.Forms.TextBox();
+            this.lstNames = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 21);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtNames
+            // 
+            this.txtNames.Location = new System.Drawing.Point(93, 21);
+            this.txtNames.Name = "txtNames";
+            this.txtNames.Size = new System.Drawing.Size(100, 23);
+            this.txtNames.TabIndex = 1;
+            // 
+            // lstNames
+            // 
+            this.lstNames.FormattingEnabled = true;
+            this.lstNames.ItemHeight = 15;
+            this.lstNames.Location = new System.Drawing.Point(12, 51);
+            this.lstNames.Name = "lstNames";
+            this.lstNames.Size = new System.Drawing.Size(181, 199);
+            this.lstNames.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstNames);
+            this.Controls.Add(this.txtNames);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnAdd;
+        private TextBox txtNames;
+        private ListBox lstNames;
     }
 }
--- Section B Lab/Section B Lab/Form1.cs
+++ Section B Lab/Section B Lab/Form1.cs
@@ -6,5 +6,51 @@ namespace Section_B_Lab
         {
             InitializeComponent();
         }
+
+        private void LoadData()
+        {
+            
+        }
+
+        private void SaveData()
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+            lstNames.Items.Add(txtNames.Text);
+
+             //string[] NamesArray = new string[2];
+            
+            try
+            {
+                StreamWriter writer = File.CreateText("Names.txt");
+                foreach(string item in lstNames.Items)
+                {
+                    writer.WriteLine(item);
+                }
+                writer.Close();
+
+                
+
+                //lstNames.Items.Add(NamesArray);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            //using(StreamWriter writetext = new StreamWriter("Names.txt"))
+            //{
+            //    writetext.WriteLine(txtNames.Text);
+            //}
+
+            
+
+        }
+        //for (int i = 0; i < NamesArray.Length; i++)
+        //            outputfile.WriteLine(NamesArray[i]);
     }
 }
