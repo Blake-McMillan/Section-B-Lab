b68dc5b9e68b977308867cc4553d559cb7ad8f47
Blake McMillan <Blake.McMillan@mymhc.ca>
2023-01-19 11:36:39 -0700

    First Half complete, Starting second half

--- Section B Lab/Section B Lab/Form1.Designer.cs
+++ Section B Lab/Section B Lab/Form1.Designer.cs
@@ -31,13 +31,22 @@
             this.btnAdd = new System.Windows.Forms.Button();
             this.txtNames = new System.Windows.Forms.TextBox();
             this.lstNames = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnWeather = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtAverageTemp = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnAdd
             // 
             this.btnAdd.Location = new System.Drawing.Point(12, 21);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.Size = new System.Drawing.Size(65, 23);
             this.btnAdd.TabIndex = 0;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -45,9 +54,9 @@
             // 
             // txtNames
             // 
-            this.txtNames.Location = new System.Drawing.Point(93, 21);
+            this.txtNames.Location = new System.Drawing.Point(83, 21);
             this.txtNames.Name = "txtNames";
-            this.txtNames.Size = new System.Drawing.Size(100, 23);
+            this.txtNames.Size = new System.Drawing.Size(137, 23);
             this.txtNames.TabIndex = 1;
             // 
             // lstNames
@@ -56,19 +65,106 @@
             this.lstNames.ItemHeight = 15;
             this.lstNames.Location = new System.Drawing.Point(12, 51);
             this.lstNames.Name = "lstNames";
-            this.lstNames.Size = new System.Drawing.Size(181, 199);
+            this.lstNames.Size = new System.Drawing.Size(208, 199);
             this.lstNames.TabIndex = 2;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 256);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(208, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear list";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnWeather
+            // 
+            this.btnWeather.Location = new System.Drawing.Point(316, 25);
+            this.btnWeather.Name = "btnWeather";
+            this.btnWeather.Size = new System.Drawing.Size(152, 23);
+            this.btnWeather.TabIndex = 4;
+            this.btnWeather.Text = "Load  2021 Weather Data";
+            this.btnWeather.UseVisualStyleBackColor = true;
+            this.btnWeather.Click += new System.EventHandler(this.btnWeather_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 80);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Challenge";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // txtAverageTemp
+            // 
+            this.txtAverageTemp.Location = new System.Drawing.Point(417, 51);
+            this.txtAverageTemp.Name = "txtAverageTemp";
+            this.txtAverageTemp.Size = new System.Drawing.Size(301, 23);
+            this.txtAverageTemp.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(474, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(244, 23);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(474, 145);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(244, 23);
+            this.textBox3.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(316, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Average temp = ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(316, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Average Temp in January = ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(316, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 15);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Average Temp in August = ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtAverageTemp);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnWeather);
+            this.Controls.Add(this.btnClear);
             this.Controls.Add(this.lstNames);
             this.Controls.Add(this.txtNames);
             this.Controls.Add(this.btnAdd);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,5 +175,14 @@
         private Button btnAdd;
         private TextBox txtNames;
         private ListBox lstNames;
+        private Button btnClear;
+        private Button btnWeather;
+        private Button button2;
+        private TextBox txtAverageTemp;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- Section B Lab/Section B Lab/Form1.cs
+++ Section B Lab/Section B Lab/Form1.cs
@@ -9,21 +9,20 @@ namespace Section_B_Lab
 
         private void LoadData()
         {
-            
-        }
-
-        private void SaveData()
-        {
-
+            //grabs and reads data from Names.txt to display in the Listbox
+            StreamReader reader = File.OpenText("Names.txt");
+            while (reader.EndOfStream == false)
+            {
+                string item = reader.ReadLine();
+                lstNames.Items.Add(item);
+            }
+            reader.Close();
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-
-            lstNames.Items.Add(txtNames.Text);
-
-             //string[] NamesArray = new string[2];
-            
+            //saves name typed in textbox to Names.txt and also displays the name in the Listbox
+            lstNames.Items.Add(txtNames.Text);        
             try
             {
                 StreamWriter writer = File.CreateText("Names.txt");
@@ -32,25 +31,71 @@ namespace Section_B_Lab
                     writer.WriteLine(item);
                 }
                 writer.Close();
-
-                
-
-                //lstNames.Items.Add(NamesArray);
             }
             catch(Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
-
-            //using(StreamWriter writetext = new StreamWriter("Names.txt"))
-            //{
-            //    writetext.WriteLine(txtNames.Text);
-            //}
-
-            
-
         }
-        //for (int i = 0; i < NamesArray.Length; i++)
-        //            outputfile.WriteLine(NamesArray[i]);
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstNames.Items.Clear();
+            //clears all names from the listbox and from Names.txt 
+            using (StreamWriter sw = new StreamWriter("Names.txt", false))
+            {
+                foreach (string line in lstNames.Items)
+                {
+                    sw.Write(""); 
+                }
+                sw.Close();
+            }          
+        }
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Loads data from Names.txt on load to display in the Listbox
+            LoadData();
+        }
+
+        private void CalcAverage()
+        {
+            StreamReader inputfile = File.OpenText("mh_temp2021.txt");
+            List<string> dgList = new List<string>();
+
+            while (!inputfile.EndOfStream)
+                dgList.Add(inputfile.ReadLine());
+
+            inputfile.Close();
+
+
+
+
+
+            //StreamReader reader = File.OpenText("mh_temp2021.txt");
+            //while (reader.EndOfStream == false)
+            //{
+            //    string item = reader.ReadLine();
+
+            //}
+            //reader.Close();
+        }
+
+
+
+
+
+
+
+        private void btnWeather_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = File.OpenText("mh_temp2021.txt");
+            while (reader.EndOfStream == false)
+            {
+                string item = reader.ReadLine();
+                txtAverageTemp.Text = item;
+            }
+            reader.Close();
+        }
     }
 }
